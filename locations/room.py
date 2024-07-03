2f4397e153560895cacd90d2d1828b8577b5794f
Ilya <rinne.ilya@gmail.com>
2024-07-03 12:21:16 +0300

    Update gitignore

--- locations/room.py
+++ locations/room.py
@@ -1,3 +1,5 @@
+import random
+
 def enter(bot, user, all_users, location):
     bot.send_message(user["id"], "Вы зашли в комнату")
 
@@ -5,6 +7,11 @@ def leave(bot, user, all_users, location):
     pass
 
 def message(bot, message, user, all_users, location):
+    if message == "/search":
+        you_found = {
+
+        }
+        bot.send_message(user["id"], "")
     bot.send_message(user["id"], "Комната")
 
 def events(bot, all_users, location):
