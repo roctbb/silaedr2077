fd123ea4a32e0d6f86655f998cd4109b3a73297a
roctbb <roctbb@gmail.com>
2024-07-04 11:25:33 +0300

    Refactor code into separate modules for storage and helpers
    The 'silaedr2077.py' script has been refactored to separate parts of the functionality into separate modules: 'storage.py' and 'helpers.py'. User data, location data and helper functions related to user management and message handling have been moved to these new files. This change aims to make the main script more readable and the overall code structure more modular and manageable.

--- silaedr2077.py
+++ silaedr2077.py
@@ -2,66 +2,10 @@ import telebot
 from config import TOKEN
 import random
 from locations import room, street, balcony, basement, forest, swamp
+from storage import *
+from helpers import *
 
 bot = telebot.TeleBot(TOKEN)
-
-users = {}
-locations = {
-    "room": {
-
-    },
-    "street": {
-
-    },
-    "balcony": {
-
-    },
-    "basement": {
-
-    },
-    "forest": {
-
-    },
-    "swamp":{
-
-    }
-}
-
-modules = {
-    "room": room,
-    "balcony": balcony,
-    "street": street,
-    "basement": basement,
-    "swamp": swamp, 
-    "forest" : forest
-}
-
-def add_user(message):
-    name = ""
-    if message.from_user.first_name != None:
-        name += message.from_user.first_name
-    else:
-        name += "Anonim"
-    if message.from_user.last_name != None:
-        name += " "
-        name += message.from_user.last_name
-    users[message.from_user.id] = {
-        "id": message.from_user.id,
-        "name": name,
-        "cookies": random.randint(10, 60),
-        "food": random.randint(50, 100),
-        "water": random.randint(50, 100),
-        "corners": 4,
-        "knowledge": 0,
-        "reputation": random.randint(30, 60),
-        "fun": random.randint(80, 100),
-        "inventory": ["laptop", "phone", "bottle", "badge"],
-        "location": "room",
-    }
-
-
-def is_registered(message):
-    return message.from_user.id in users
 
 @bot.message_handler(content_types=['text'])
 def process_message(message):
@@ -71,20 +15,21 @@ def process_message(message):
     user = users[message.from_user.id]
 
     if message.text.startswith("/") and message.text.strip('/') in locations:
-        module = modules[user["location"]]
-        all_users = list(filter(lambda x: x["location"] == user["location"], users.values()))
+        module = get_module(user)
+        all_users = get_neighbours(user)
+
         module.leave(bot, user, all_users)
 
 
         location_name = message.text.strip('/')
         user["location"] = location_name
 
-        module = modules[user["location"]]
-        all_users = list(filter(lambda x: x["location"] == user["location"], users.values()))
+        module = get_module(user)
+        all_users = get_neighbours(user)
         module.enter(bot, user, all_users)
     else:
-        module = modules[user["location"]]
-        all_users = list(filter(lambda x: x["location"] == user["location"], users.values()))
+        module = get_module(user)
+        all_users = get_neighbours(user)
 
         module.message(bot, message, user, all_users)
 
---
+++ storage.py
@@ -0,0 +1,30 @@
+users = {}
+locations = {
+    "room": {
+
+    },
+    "street": {
+
+    },
+    "balcony": {
+
+    },
+    "basement": {
+
+    },
+    "forest": {
+
+    },
+    "swamp":{
+
+    }
+}
+
+modules = {
+    "room": room,
+    "balcony": balcony,
+    "street": street,
+    "basement": basement,
+    "swamp": swamp,
+    "forest" : forest
+}
