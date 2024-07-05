10dcb10dee6f915431813136171dc1b194351973
Roma <gagaga3859@gmail.com>
2024-07-05 16:44:33 +0300

    finished save sistem

--- silaedr2077.py
+++ silaedr2077.py
@@ -4,32 +4,25 @@ from helpers import *
 
 bot = get_bot()
 
-load_data()
+@bot.message_handler(content_types=['text'])
+def process_message(message):
+    if not is_registered(message):
+        add_user(message)
 
-try:
-    @bot.message_handler(content_types=['text'])
-    def process_message(message):
-        if not is_registered(message):
-            add_user(message)
+    user = users[str(message.from_user.id)]
 
-        user = users[message.from_user.id]
+    if message.text == "/locations":
+        bot.send_message(user["id"], "/" + '\n/'.join(locations.keys()))
+    elif message.text == "/stats":
+        give_stats(user, bot)
+    elif message.text.startswith("/") and message.text.strip('/') in locations:
+        move_player(bot, user, message.text.strip('/'))
+    else:
+        module = get_module(user)
+        all_users = get_neighbours(user)
 
-        if message.text == "/locations":
-            bot.send_message(user["id"], "/" + '\n/'.join(locations.keys()))
-        elif message.text == "/stats":
-            give_stats(user, bot)
-        elif message.text.startswith("/") and message.text.strip('/') in locations:
-            move_player(bot, user, message.text.strip('/'))
-        else:
-            module = get_module(user)
-            all_users = get_neighbours(user)
+        module.message(bot, message, user, all_users, locations[user['location']])
 
-            module.message(bot, message, user, all_users,
-                        locations[user['location']])
+    save_data()
 
-
-    bot.polling(none_stop=True)
-except:
-    pass
-
-save_data()
+bot.polling(none_stop=True)
